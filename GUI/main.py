5e818cc2b0bfe005c379d12d4c3dfea9cf4c15aa
tomyummmm <mervin989@gmail.com>
2022-03-09 13:53:03 +0800

    IPv4 Validation

--- GUI/main.py
+++ GUI/main.py
@@ -8,12 +8,13 @@
 
 import sys
 import platform
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent, QSortFilterProxyModel)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+import zmq
+# from PySide2 import QtCore, QtGui, QtWidgets
+# from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent, QSortFilterProxyModel, QSettings)
 from PySide2.QtWidgets import *
-from os.path import exists
 from PySide2.QtSql import *
+from os.path import exists
 
 
 # GUI FILE
@@ -60,8 +61,8 @@ class MainWindow(QMainWindow):
 		self.model.setHeaderData(2, Qt.Horizontal, "Read Count")
 		self.model.setHeaderData(3, Qt.Horizontal, "Last Read From")
 		self.model.setHeaderData(4, Qt.Horizontal, "First Seen (Y-M-D H:M:S)")
-		self.model.setHeaderData(5, Qt.Horizontal, "Last Seen")
-		self.model.setHeaderData(6, Qt.Horizontal, "Time Since Last Seen (Sec)")
+		self.model.setHeaderData(5, Qt.Horizontal, "Last Seen (Y-M-D H:M:S)")
+		self.model.setHeaderData(6, Qt.Horizontal, "Time Since \nLast Seen (Sec)")
 		self.model.setHeaderData(7, Qt.Horizontal, "Last RSSI")
 		self.model.setHeaderData(8, Qt.Horizontal, "RSSI Avg")
 		self.model.setHeaderData(9, Qt.Horizontal, "RSSI Max")
@@ -73,7 +74,7 @@ class MainWindow(QMainWindow):
 		# Update tableView as QSqlTableModel
 		self.ui.tableView.setModel(self.model)
 		self.ui.tableView.resizeColumnsToContents()
-		# # Set to stretch EPC column so that entire table stays in view regardless of window size
+		# Set to stretch EPC column so that entire table stays in view regardless of window size
 		self.ui.tableView.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
 
 		# Setup QSortFilterProxyModel to enable search through QSqlTableModel
@@ -91,6 +92,12 @@ class MainWindow(QMainWindow):
 
 		# Trigger db row removal
 		self.ui.remove_row.clicked.connect(self.DeleteRow)
+		
+		# Retrieve Settings from QSettings
+		self.getSettingsValues()
+		
+		# Trigger IP Address change after edit
+		self.ui.lineEdit_ip.editingFinished.connect(self.Edit_IP)
 
 		# MOVE WINDOW
 		def moveWindow(event):
@@ -143,32 +150,58 @@ class MainWindow(QMainWindow):
 		# Power INTEGER
 		# Phase_Angle INTEGER
 		# Doppler_Freq INTEGER
+
 		# Using SQL UPSERT (https://www.sqlite.org/lang_UPSERT.html). Special syntax addition to INSERT that causes the INSERT to behave as an UPDATE or a no-op if the INSERT would violate a uniqueness constraint. UPSERT is not standard SQL.
-		query.exec_("""INSERT INTO projects (EPC, Category, Read_Count, Last_Read_From, First_Seen, Last_Seen, Time_Since_Last_Seen, Last_RSSI, RSSI_Avg, RSSI_Max, RSSI_Min, Power, Phase_Angle, Doppler_Freq)
+		query.prepare("""INSERT INTO projects (EPC, Category, Read_Count, Last_Read_From, First_Seen, Last_Seen, Time_Since_Last_Seen, Last_RSSI, RSSI_Avg, RSSI_Max, RSSI_Min, Power, Phase_Angle, Doppler_Freq)
 					VALUES('912391239123912391239123', 'Classic', 0, '127.0.0.1', '2022-01-01 12:00:00.001', '2022-01-02 12:00:00.001', 2678400, -50.30, -48.8, -45.2, -54.6, 30.00, NULL, NULL)
 					ON CONFLICT (EPC) DO
 					UPDATE SET Read_Count=Read_count+1,
 					Time_Since_Last_Seen=(julianday('now') - julianday(Last_Seen)) * 86400,
-					Last_Seen=strftime('%Y-%m-%d %H:%M:%f', 'now')
+					Last_Seen=strftime('%Y-%m-%d %H:%M:%f', 'now'),
+					Last_Read_From=?
 					""")
+		# Substitute positional bind value to ? for Last_Read_From, prepare because cannot execute directly with string literal.
+		query.addBindValue(self.ui.lineEdit_ip.text())
+		query.exec_()
 		self.model.select()
 		# self.model.submitAll()
-
-		# self.db.exec_("""INSERT INTO projects (EPC, Category, Read_Count, Last_Read_From, First_Seen, Last_Seen, Time_Since_Last_Seen, Last_RSSI, RSSI_Avg, RSSI_Max, RSSI_Min, Power, Phase_Angle, Doppler_Freq)
-		# 			VALUES('912391239123912391239123', 'Classic', 0, '127.0.0.1', '2022-01-01 12:00:00.001', '2022-01-02 12:00:00.001', 2678400, -50.30, -48.8, -45.2, -54.6, 30.00, NULL, NULL)
-		# 			ON CONFLICT (EPC) DO UPDATE SET 
-		# 			Read_Count=Read_count+1,
-		# 			Time_Since_Last_Seen=strftime('%Y-%m-%d %H:%M:%f', 'now') - strftime('%Y-%m-%d %H:%M:%f', Last_Seen),
-		# 			Last_Seen=strftime('%Y-%m-%d %H:%M:%f', 'now')
-		# 			""")
-		# self.model.submitAll()
-
 
 	def DeleteRow(self):
 		self.model.removeRow(self.ui.tableView.currentIndex().row())
 		self.model.select()
 		# self.model.submitAll()
 
+	def Edit_IP(self):
+		error = 0
+		# check for valid IPv4 Address, check 4 sub-fields, int range 0 to 255 (8 bits).
+		ip_split = self.ui.lineEdit_ip.text().split('.')
+		if len(ip_split) != 4:
+			error = 1
+		for i in ip_split:
+			if i.isdigit() and int(i) <= 255 and int(i) >= 0:
+				pass
+			else:
+				error = 1
+				break
+		if error == 1:
+			error_dialog = QMessageBox()
+			error_dialog.setIcon(QMessageBox.Critical)
+			error_dialog.setText("Invalid IPv4 Address")
+			error_dialog.setInformativeText('IP Address must follow IPv4 standards')
+			error_dialog.setWindowTitle("Error")
+			# error_dialog.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+			error_dialog.exec_()
+			self.ui.lineEdit_ip.clear()
+
+		# Set Value of QSettings 'IP_address'
+		self.setting_ip.setValue('IP_address', self.ui.lineEdit_ip.text())
+		# self.model.select()
+
+	def getSettingsValues(self):
+		self.setting_ip = QSettings('GUI Database', 'IP Address')
+		self.ui.lineEdit_ip.setText(self.setting_ip.value('IP_Address'))
+
+		# self.setting_window = QSettings('GUI Database', 'Window Size')
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
