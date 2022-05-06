39459c774d60ff57a15a648a72d1df746cd6001a
tomyummmm <mervin989@gmail.com>
2022-05-07 00:27:43 +0800

    Cleanup WSL_Launcher.bat

--- WSL_Launcher.bat
+++ WSL_Launcher.bat
@@ -1,7 +1,3 @@
 FOR /F "tokens=1" %%i IN ('usbipd wsl list ^| findstr /c:"LimeSDR-USB"') DO SET VARIABLE=%%i
 
 wt -p "Ubuntu-20.04" ; new-tab -p "Command Prompt" cmd /k "usbipd wsl list & usbipd wsl attach --busid %VARIABLE%"
-::wt -p "Ubuntu-20.04" ; new-tab -p "Command Prompt" cmd /k "usbipd wsl list & usbipd wsl attach --busid %VARIABLE%"
-
-:: usbipd wsl list | findstr /c:"LimeSDR-USB"
-::cmd /k
